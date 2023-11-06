92d02d639a29034868257ce74bc855fe7a7532d9
amari <amari.lawal@gmail.com>
2023-11-06 20:18:37 +0000

    CaesarAIEngineBackend online

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "CaesarAIEngineBackend online"
-git push origin -f master:main
+git push origin -f main:main
 docker build -t palondomus/caesaraiengineworld:latest .
 docker push palondomus/caesaraiengineworld:latest
 docker run -it -p 8080:8080 palondomus/caesaraiengineworld:latest
